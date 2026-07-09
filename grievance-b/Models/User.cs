9458250c3a62ca91c9f309ce2632649e8d0c1a3b
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-09 22:00:34 +0530

    Added basic user DB struct(Model)

---
+++ grievance-b/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace grievance_b.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public string ProfilePicturePath { get; set; } = string.Empty;
+    }
+}
