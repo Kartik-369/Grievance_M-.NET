341e532e8c1c52f1a40b8e021370a4c6f610a9d9
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-10 19:21:06 +0530

    Roles Table

---
+++ grievance-b/Models/Roles.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace grievance_b.Models
+{
+    public class Roles
+    {
+        [Key]
+        public int RoleId { get; set; }
+        public string RoleName { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
