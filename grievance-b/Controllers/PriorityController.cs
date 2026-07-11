1a4e116e38a99105851a26a9df35713554fa49b9
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-11 11:20:20 +0530

    Controller added for same Priority

---
+++ grievance-b/Controllers/PriorityController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using grievance_b.Data;
+using grievance_b.Models;
+
+namespace grievance_b.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriorityController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PriorityController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPriorities()
+        {
+            return Ok(await _context.Priority.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPriorityById(int id)
+        {
+            var priority = await _context.Priority.FindAsync(id);
+            return priority == null ? NotFound() : Ok(priority);
+        }
+    }
+}
