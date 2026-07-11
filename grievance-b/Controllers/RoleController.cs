d164dc5f1706c468bafea77760b92946565ce8a1
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-11 11:20:37 +0530

    Controller added for Role

---
+++ grievance-b/Controllers/RoleController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using grievance_b.Data;
+using grievance_b.Models;
+
+namespace grievance_b.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        #region Dependency Injected
+        private readonly AppDbContext _context;
+
+        public RoleController(AppDbContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Get All Roles
+        // GET: api/role
+        [HttpGet]
+        public async Task<IActionResult> GetAllRoles()
+        {
+            var roles = await _context.Roles.ToListAsync();
+            return Ok(roles);
+        }
+        #endregion
+
+        #region Get Role By ID
+        // GET: api/role/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetRoleById(int id)
+        {
+            var role = await _context.Roles.FindAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(role);
+        }
+        #endregion
+    }
+}
