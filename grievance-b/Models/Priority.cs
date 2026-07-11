823a06e9e414d4fe22d87a4829f1589536b1d97e
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-11 11:19:32 +0530

    Added DB Model for priority

---
+++ grievance-b/Models/Priority.cs
@@ -0,0 +1,9 @@
+﻿namespace grievance_b.Models
+{
+    public class Priority
+    {
+        public int PriorityId { get; set; }
+        public string PriorityName { get; set; } = string.Empty;
+        public string PriorityCssClass { get; set; } = string.Empty;
+    }
+}
