b3270c00515e8be97f1232a9bd453a3e85a46b4b
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-12 15:02:21 +0530

    Added Grievance Stat History

---
+++ grievance-b/Models/GrievanceStatusHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace grievance_b.Models
+{
+    public class GrievanceStatusHistory
+
+    {
+        [Key]
+        public int StatusHistoryId { get; set; }
+        public int GrievanceId { get; set; }
+        public int Status { get; set; }
+        public string? Remarks { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; } = DateTime.Now;
+    }
+}
