881c4c067770b3c0873bc364e3c89ee3b937c0f7
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-12 15:03:02 +0530

    added grievance model DB

---
+++ grievance-b/Models/Grievances.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace grievance_b.Models
+{
+    public class Grievances
+
+    {
+        [Key]
+        public int GrievanceId { get; set; }
+        public int RaisedBy { get; set; }
+        public int CategoryId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Status { get; set; }
+        public int Priority { get; set; }
+    }
+}
