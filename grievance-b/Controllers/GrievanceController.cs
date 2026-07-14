6d9b339ec11735e79cc7ec57fc8c6db26432ec99
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-14 12:00:00 +0530

    added controller for grievance

---
+++ grievance-b/Controllers/GrievanceController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using grievance_b.Data;
+using grievance_b.Models;
+
+namespace grievance_b.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GrievanceController : ControllerBase
+    {
+        #region Dependency Injected
+        private readonly AppDbContext _context;
+
+        public GrievanceController(AppDbContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Get All Grievances
+        // GET: api/grievance
+        [HttpGet]
+        public async Task<IActionResult> GetAllGrievances()
+        {
+            var grievances = await _context.Grievances.ToListAsync();
+            return Ok(grievances);
+        }
+        #endregion
+
+        #region Get Grievance By ID
+        // GET: api/grievance/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGrievanceById(int id)
+        {
+            var grievance = await _context.Grievances.FindAsync(id);
+
+            if (grievance == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(grievance);
+        }
+        #endregion
+    }
+}
