134dbe504668ce85e714655fc19a632cc2fa050b
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-15 08:40:55 +0530

    Added DB for grievance categories

---
+++ grievance-b/Models/GrievanceCategories.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace grievance_b.Models
+{
+    public class GrievanceCategories
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string CssClassName { get; set; } = string.Empty;
+    }
+}
