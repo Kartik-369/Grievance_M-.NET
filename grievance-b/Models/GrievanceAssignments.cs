2a21f012855cdb4cfc9fef07fbd9fae6396b3027
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-16 21:29:48 +0530

    Added DB for grievance assignments

---
+++ grievance-b/Models/GrievanceAssignments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace grievance_b.Models
+{
+    public class GrievanceAssignments
+
+    {
+        [Key]
+        public int AssignmentId { get; set; }
+        public int GrievanceId { get; set; }
+        public int AssignedTo { get; set; }
+        public DateTime AssignedAt { get; set; } = DateTime.Now;
+    }
+}
