9e5a3be52d61b3db6c870fd50419052b18c110e2
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-17 12:00:00 +0530

    \n

---
+++ grievance-b/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using grievance_b.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace grievance_b.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Roles> Roles => Set<Roles>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<GrievanceCategories> GrievanceCategories => Set<GrievanceCategories>();
+        public DbSet<Status> Status => Set<Status>();
+        public DbSet<Priority> Priority => Set<Priority>();
+        public DbSet<Grievances> Grievances => Set<Grievances>();
+        public DbSet<GrievanceAssignments> GrievanceAssignments => Set<GrievanceAssignments>();
+        public DbSet<GrievanceStatusHistory> GrievanceStatusHistory => Set<GrievanceStatusHistory>();
+    }
+}
