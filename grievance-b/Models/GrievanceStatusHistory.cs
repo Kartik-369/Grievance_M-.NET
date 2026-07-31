01af2a3e5fcde0358ede391811e6f0b6256d2ce3
kartik-369 <kartikbpadia0707@gmail.com>
2026-07-31 12:00:00 +0530

    minor fixes in naming

--- grievance-b/Models/GrievanceStatusHistory.cs
+++ grievance-b/Models/GrievanceStatusHistory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,14 +11,14 @@ namespace grievance_b.Models
         public int StatusHistoryId { get; set; }
         [ForeignKey("Grievances")]
         public int GrievanceId { get; set; }
-        [ForeignKey("status")]
+        [ForeignKey("StatusNavigation")]
         public int Status { get; set; }
         public string? Remarks { get; set; }
         [ForeignKey("User")]
         public int UpdatedBy { get; set; }
         public DateTime UpdatedOn { get; set; } = DateTime.Now;
         public Grievances Grievances { get; set; }
-        public Status status { get; set; }
+        public Status StatusNavigation { get; set; }
         public User User { get; set; }
     }
 }
