4e9f98b46261503e687d547beaef102a16619387
kartik-369 <kartikbpadia0707@gmail.com>
2026-08-02 19:01:42 +0530

    added key for Fore_key ref

--- grievance-b/Models/Priority.cs
+++ grievance-b/Models/Priority.cs
@@ -1,7 +1,10 @@
-﻿namespace grievance_b.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace grievance_b.Models
 {
     public class Priority
     {
+        [Key]
         public int PriorityId { get; set; }
         public string PriorityName { get; set; } = string.Empty;
         public string PriorityCssClass { get; set; } = string.Empty;
