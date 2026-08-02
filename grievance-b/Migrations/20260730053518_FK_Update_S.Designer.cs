036950269945caa49923e031c49cd6e4f4930979
kartik-369 <kartikbpadia0707@gmail.com>
2026-08-02 19:08:35 +0530

    ran latest migration + previous fixes

--- grievance-b/Migrations/20260730053518_FK_Update_S.Designer.cs
+++ grievance-b/Migrations/20260730053518_FK_Update_S.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using grievance_b.Data;
 
@@ -11,9 +12,11 @@ using grievance_b.Data;
 namespace grievance_b.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260730053518_FK_Update_S")]
+    partial class FK_Update_S
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ grievance-b/Migrations/20260730053518_FK_Update_S.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace grievance_b.Migrations
+{
+    /// <inheritdoc />
+    public partial class FK_Update_S : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grievances_Priority_Priority",
+                table: "Grievances");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grievances_Status_Status",
+                table: "Grievances");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Grievances",
+                newName: "StatusId");
+
+            migrationBuilder.RenameColumn(
+                name: "Priority",
+                table: "Grievances",
+                newName: "PriorityId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Grievances_Status",
+                table: "Grievances",
+                newName: "IX_Grievances_StatusId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Grievances_Priority",
+                table: "Grievances",
+                newName: "IX_Grievances_PriorityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grievances_Priority_PriorityId",
+                table: "Grievances",
+                column: "PriorityId",
+                principalTable: "Priority",
+                principalColumn: "PriorityId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grievances_Status_StatusId",
+                table: "Grievances",
+                column: "StatusId",
+                principalTable: "Status",
+                principalColumn: "StatusID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grievances_Priority_PriorityId",
+                table: "Grievances");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Grievances_Status_StatusId",
+                table: "Grievances");
+
+            migrationBuilder.RenameColumn(
+                name: "StatusId",
+                table: "Grievances",
+                newName: "Status");
+
+            migrationBuilder.RenameColumn(
+                name: "PriorityId",
+                table: "Grievances",
+                newName: "Priority");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Grievances_StatusId",
+                table: "Grievances",
+                newName: "IX_Grievances_Status");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Grievances_PriorityId",
+                table: "Grievances",
+                newName: "IX_Grievances_Priority");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grievances_Priority_Priority",
+                table: "Grievances",
+                column: "Priority",
+                principalTable: "Priority",
+                principalColumn: "PriorityId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Grievances_Status_Status",
+                table: "Grievances",
+                column: "Status",
+                principalTable: "Status",
+                principalColumn: "StatusID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
