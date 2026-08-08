dd2b0f7e8e35a540c9903a02cd10e428771563e0
kartik-369 <kartikbpadia0707@gmail.com>
2026-08-08 12:00:00 +0530

    crud: grievance category

--- grievance-b/Controllers/GrievanceCategoryController.cs
+++ grievance-b/Controllers/GrievanceCategoryController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using grievance_b.Data;
 using grievance_b.Models;
@@ -18,20 +18,20 @@ namespace grievance_b.Controllers
         }
         #endregion
 
-        #region Get All Categories
+        #region Get All Grievance Categories
         // GET: api/grievancecategory
         [HttpGet]
-        public async Task<IActionResult> GetAllCategories()
+        public async Task<IActionResult> GetAllGrievanceCategories()
         {
             var categories = await _context.GrievanceCategories.ToListAsync();
             return Ok(categories);
         }
         #endregion
 
-        #region Get Category By ID
+        #region Get Grievance Category By ID
         // GET: api/grievancecategory/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetCategoryById(int id)
+        public async Task<IActionResult> GetGrievanceCategoryById(int id)
         {
             var category = await _context.GrievanceCategories.FindAsync(id);
 
@@ -41,6 +41,55 @@ namespace grievance_b.Controllers
             }
 
             return Ok(category);
+        }
+        #endregion
+
+        #region Create Grievance Category
+        [HttpPost]
+        public async Task<IActionResult> Create(GrievanceCategories category)
+        {
+            await _context.GrievanceCategories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        #endregion
+
+        #region Update Grievance Category
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, GrievanceCategories category)
+        {
+            if (id != category.CategoryId)
+            {
+                return BadRequest();
+            }
+            
+            var existingCategory = await _context.GrievanceCategories.FindAsync(id);
+            if (existingCategory == null)
+            {
+                return NotFound();
+            }
+
+            existingCategory.CategoryName = category.CategoryName;
+            existingCategory.CssClassName = category.CssClassName;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        #endregion
+        #region Delete Grievance Category
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _context.GrievanceCategories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.GrievanceCategories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
         #endregion
     }
