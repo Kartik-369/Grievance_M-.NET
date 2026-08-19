71411af7e21584949c504da215470d9e7a4bddd7
kartik-369 <kartikbpadia0707@gmail.com>
2026-08-19 19:16:00 +0530

    Update DB

---
+++ grievance-b/Migrations/20260818052506_DB_Update.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace grievance_b.Migrations
+{
+    /// <inheritdoc />
+    public partial class DB_Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
